39a89440585c1df077af582e467606ab13efeb22
Andrew Godfroy <andrew.godfroy@killerrin.com>
2016-01-22 16:24:04 -0500

    Initial Commit
    ASP.net blog

---
+++ Simple Blog/Simple Blog/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Simple_Blog.Controllers;
+
+namespace Simple_Blog
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            var namespaces = new[] { typeof(PostsController).Namespace };
+            routes.MapRoute("Login", "login", new { controller = "Auth", Action = "Login" }, namespaces);
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
+        }
+    }
+}
---
+++ Simple Blog/Simple Blog/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Simple_Blog.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
